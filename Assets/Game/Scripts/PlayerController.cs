1994c5427e774ad46d23c8feda1f1bdf0858eafe
AymaneAbidane <76812961+AymaneAbidane@users.noreply.github.com>
2024-07-09 16:07:53 +0100

    moving The Character Done

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,10 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movment varriables")]
+    [SerializeField] private float mvSpeed;
+    [SerializeField] private Rigidbody playerRb;
+
+    [Space]
+
+
     private PlayerInputs inputs;
+    private Vector3 movmentVec;
 
     private void Awake()
     {
@@ -18,9 +28,26 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        ReadMovmentInputs();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //playerRb.velocity = mvSpeed * Time.fixedDeltaTime * movmentVec;
+        playerRb.MovePosition(transform.position + (mvSpeed * Time.fixedDeltaTime * movmentVec));//this methode will make the movment faster than using dot velocity and the step will be pre calculated
+    }
+
+    private void ReadMovmentInputs()
+    {
         float x = inputs.PlayerInputsControlls.Move.ReadValue<Vector2>().x;
         float z = inputs.PlayerInputsControlls.Move.ReadValue<Vector2>().y;
 
-        Debug.Log(x + " , " + z);
+        movmentVec = new Vector3(x, 0f, z).normalized;
     }
 }
